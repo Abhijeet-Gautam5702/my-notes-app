d8058512f36e3c8c77de30004c1afb0924e000b2
Abhijeet <abhijeetgautam572@gmail.com>
2022-11-07 23:53:51 +0530

    updated script.js
    completed delete-btn functionality for each note

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-
 const storageDiv = document.querySelector(".storage-container");
 
 //render the previously stored notes from localStorage
@@ -17,33 +14,20 @@ const saveBtn = document.getElementById("save-btn");
 saveBtn.addEventListener("click", function () {
   const titleEl = document.getElementById("title");
   const notesEl = document.getElementById("notes");
+  const id = Math.random();
 
   const itemObject = {
+    id: id,
     title: titleEl.value,
     notes: notesEl.value,
   };
-  //fetch the array containing previous Notes from localStorage
-  const fetchPrevNotes = JSON.parse(localStorage.getItem("myNotes"));
+  //fetch the array from localStorage
+  const notes = JSON.parse(localStorage.getItem("myNotes"));
   //update the array and push it again in the localStorage
-  fetchPrevNotes.push(itemObject);
-  localStorage.setItem("myNotes", JSON.stringify(fetchPrevNotes));
+  notes.push(itemObject);
+  localStorage.setItem("myNotes", JSON.stringify(notes));
 
-  //render the latest note
-  const n = fetchPrevNotes.length;
-  storageDiv.innerHTML += 
-  `
-    <hr class = "divider" />
-    <div class = "note-container">
-      <div class = "note-content-container">
-        <h3 class = "note-title"> ${fetchPrevNotes[n - 1].title} </h3>
-        <p class = "note-content">${fetchPrevNotes[n - 1].notes}</p>
-      </div>
-      <div class = "control-btn-container">
-        <button class = "dlt-btn">delete</button>
-      </div>
-    </div>
-  `;
-  // storageDiv.classList.toggle("container-css");
+  render(notes);
 
   //clear the input boxes after save-btn is clicked
   titleEl.value = "";
@@ -68,8 +52,8 @@ function render(arr) {
   let x = "";
   for (let i = 0; i < arr.length; i++) {
     x += `
-    <hr class = "divider" />
-    <div class = "note-container">
+    
+    <div class = "note-container" data-id = ${arr[i].id}>
       <div class = "note-content-container">
         <h3 class = "note-title"> ${arr[i].title} </h3>
         <p class = "note-content">${arr[i].notes}</p>
@@ -81,7 +65,23 @@ function render(arr) {
   }
   storageDiv.innerHTML = x;
   storageDiv.classList.add("container-css");
+
+  //add event listener for delete-btn
+  const dltBtns = storageDiv.querySelectorAll(".dlt-btn");
+  for (let i = 0; i < dltBtns.length; i++) {
+    dltBtns[i].addEventListener("click", deleteItem);
+  }
 }
 
-
-
+function deleteItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  storageDiv.removeChild(element);
+  //delete item from localStorage
+  const items = JSON.parse(localStorage.getItem("myNotes"));
+  for (let i = 0; i < items.length; i++) {
+    if (element.getAttribute("data-id") == items[i].id) {
+      items.splice(i, 1);
+      localStorage.setItem("myNotes", JSON.stringify(items));
+    }
+  }
+}
