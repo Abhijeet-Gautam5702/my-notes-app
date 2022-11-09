1534b8349bba7b46ef5eea3acfd9b17ad55002f4
Abhijeet <abhijeetgautam572@gmail.com>
2022-11-09 13:46:02 +0530

    updated script.js and style.css
    1. updated UI
    2. added delete-note and edit-note functionality using JS

--- script.js
+++ script.js
@@ -11,24 +11,29 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const saveBtn = document.getElementById("save-btn");
+const titleEl = document.getElementById("title");
+const notesEl = document.getElementById("notes");
 saveBtn.addEventListener("click", function () {
-  const titleEl = document.getElementById("title");
-  const notesEl = document.getElementById("notes");
-  const id = Math.random();
+  if (!editFlag) {
+    const id = Math.random();
 
-  const itemObject = {
-    id: id,
-    title: titleEl.value,
-    notes: notesEl.value,
-  };
-  //fetch the array from localStorage
-  const notes = JSON.parse(localStorage.getItem("myNotes"));
-  //update the array and push it again in the localStorage
-  notes.push(itemObject);
-  localStorage.setItem("myNotes", JSON.stringify(notes));
+    const itemObject = {
+      id: id,
+      title: titleEl.value,
+      notes: notesEl.value,
+    };
+    //fetch the array from localStorage
+    const notes = JSON.parse(localStorage.getItem("myNotes"));
+    //update the array and push it again in the localStorage
+    notes.push(itemObject);
+    localStorage.setItem("myNotes", JSON.stringify(notes));
 
-  render(notes);
-
+    render(notes);
+  } else {
+    editLocalStorage(editID, titleEl.value, notesEl.value);
+    editFlag = false;
+    saveBtn.textContent = "Save";
+  }
   //clear the input boxes after save-btn is clicked
   titleEl.value = "";
   notesEl.value = "";
@@ -55,33 +60,71 @@ function render(arr) {
     
     <div class = "note-container" data-id = ${arr[i].id}>
       <div class = "note-content-container">
-        <h3 class = "note-title"> ${arr[i].title} </h3>
+        <h3 class = "note-title">${arr[i].title}</h3>
         <p class = "note-content">${arr[i].notes}</p>
       </div>
       <div class = "control-btn-container">
-        <button class = "dlt-btn">delete</button>
+        <img class = "edit-icon" src = "edit-icon.png" alt="edit">
+        <img class = "dlt-icon" src = "dlt-icon.png" alt="dlt">
       </div>
     </div>`;
   }
   storageDiv.innerHTML = x;
   storageDiv.classList.add("container-css");
 
-  //add event listener for delete-btn
-  const dltBtns = storageDiv.querySelectorAll(".dlt-btn");
+  //add event listener for edit/delete-btns
+  const dltBtns = storageDiv.querySelectorAll(".dlt-icon");
+  const editBtns = storageDiv.querySelectorAll(".edit-icon");
   for (let i = 0; i < dltBtns.length; i++) {
     dltBtns[i].addEventListener("click", deleteItem);
+  }
+  for (let i = 0; i < editBtns.length; i++) {
+    editBtns[i].addEventListener("click", editItem);
   }
 }
 
 function deleteItem(e) {
   const element = e.currentTarget.parentElement.parentElement;
+  const id = element.getAttribute("data-id");
   storageDiv.removeChild(element);
   //delete item from localStorage
-  const items = JSON.parse(localStorage.getItem("myNotes"));
-  for (let i = 0; i < items.length; i++) {
-    if (element.getAttribute("data-id") == items[i].id) {
-      items.splice(i, 1);
-      localStorage.setItem("myNotes", JSON.stringify(items));
+  let items = JSON.parse(localStorage.getItem("myNotes"));
+  items = items.filter(function (item) {
+    if (item.id != id) {
+      return item;
     }
-  }
+  });
+  localStorage.setItem("myNotes", JSON.stringify(items));
+}
+
+let editFlag = false;
+let editID = "";
+function editItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.getAttribute("data-id");
+
+  const title = element.querySelector(".note-title").textContent;
+  const note = element.querySelector(".note-content").textContent;
+
+  titleEl.value = title;
+  notesEl.value = note;
+  saveBtn.textContent = "Edit";
+  editFlag = true;
+  editID = id;
+}
+
+function editLocalStorage(id, title, note) {
+  let items = JSON.parse(localStorage.getItem("myNotes"));
+  // console.log(items);
+  items = items.filter(function (item) {
+    if (item.id != id) {
+      return item;
+    } else if (item.id == id) {
+      item.title = title;
+      item.notes = note;
+      return item;
+    }
+  });
+  localStorage.setItem("myNotes", JSON.stringify(items));
+  render(items);
 }
